f67a25b37798842d5714dd6258714b35acbddadb
facu-pirolo <49518308@est.ort.edu.ar>
2025-05-12 10:48:24 -0300

    clone random

---
+++ Assets/Scrpits/RandomCloner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCloner : MonoBehaviour
+{
+    public int randomNumber;
+    public int min;
+    public int max;
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CloneRandom()
+    {
+        randomNumber = Random.Range(min, max + 1);
+        GameObject clone = Prefab2;
+        if (randomNumber == 0)
+        {
+            clone = Prefab1;
+        }
+        else if(randomNumber == 1)
+        {
+            clone = Prefab2;
+        }
+        else if (randomNumber == 2)
+        {
+            clone = Prefab3;
+        }
+        Instantiate(clone);
+    }
+}

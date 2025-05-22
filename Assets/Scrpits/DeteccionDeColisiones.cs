394ce925d4e46d84e2bbe4013b5b680ae6248076
facu-pirolo <49518308@est.ort.edu.ar>
2025-05-22 10:43:10 -0300

    Collisiones

---
+++ Assets/Scrpits/DeteccionDeColisiones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+        if (col.gameObject. name == "Player")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}

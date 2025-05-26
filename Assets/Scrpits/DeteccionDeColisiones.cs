1b2c549b74c23d05a7161124135ffa3c8749b293
facu-pirolo <49518308@est.ort.edu.ar>
2025-05-26 09:56:48 -0300

    sumar puntos

--- Assets/Scrpits/DeteccionDeColisiones.cs
+++ Assets/Scrpits/DeteccionDeColisiones.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class DeteccionDeColisiones : MonoBehaviour
 {
+    public ScoreManager scoreManager;
+
+    void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+    }
+
     void OnCollisionEnter(Collision col)
     {
         Debug.Log("Contacto");
         if (col.gameObject. name == "Player")
         {
             Destroy(gameObject);
+            scoreManager.AddScore();
         }
         
     }
---
+++ Assets/Scrpits/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+   public void AddScore()
+    {
+        score ++;
+    }
+}

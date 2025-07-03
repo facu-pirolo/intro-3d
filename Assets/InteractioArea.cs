d8c5c45528a66154fcd5f882a1a0c2e9111e8433
facu-pirolo <49518308@est.ort.edu.ar>
2025-07-03 10:33:06 -0300

    caja e destruir

---
+++ Assets/InteractioArea.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractioArea : MonoBehaviour
+{
+    public GameObject UIInteraxtionMessage;
+    public bool canInteract;
+    public MercaderiaScripts mercaderia;
+
+    private void Start()
+    {
+        UIInteraxtionMessage.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (canInteract)
+            {
+                Destroy(mercaderia.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+         mercaderia = other.GetComponent<MercaderiaScripts>();
+        if (mercaderia)
+        {
+            UIInteraxtionMessage.SetActive(true);
+            canInteract = true;
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        UIInteraxtionMessage.SetActive(false);
+        mercaderia = null;
+        canInteract = false;
+    }
+  
+    
+}
